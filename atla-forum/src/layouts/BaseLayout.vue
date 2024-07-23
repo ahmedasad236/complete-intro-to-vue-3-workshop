2726fb1f8f2700bc28811c4fb3a4a757c8f8748c
ahmedasad236 <ahmedasad236@gmail.com>
2024-07-23 14:06:23 +0300

    Create BaseLayout using Slots

---
+++ atla-forum/src/layouts/BaseLayout.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: "BaseLayout",
+};
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot name="main" />
+    </main>
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+header {
+  background-color: #f0f0f0;
+  padding: 1rem;
+}
+
+main {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+footer {
+  background-color: #f0f0f0;
+  padding: 1rem;
+}
+</style>
